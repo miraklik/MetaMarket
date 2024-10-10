b0933419a71e7aa01f49792ed868731dd7cc88c6
mirak <karmau473@gmail.com>
2024-10-10 19:16:09 +0300

    add payments.sol and test on js

--- smart-contract/contracts/payments.sol
+++ smart-contract/contracts/payments.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Payments {
+
     struct Payment {
         uint amount;
         uint timestamp;
@@ -16,22 +17,26 @@ contract Payments {
 
     mapping(address => Balance) public balances;
 
+    function currentBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
     function getPayment(address _addr, uint _index) public view returns(Payment memory) {
+        require(_index < balances[_addr].totalPayments, "Payment does not exist");
         return balances[_addr].payments[_index];
     }
 
     function pay(string memory message) public payable {
+        require(msg.value < 0, "there are no funds in the wallet");
+
         uint paymentNum = balances[msg.sender].totalPayments;
         balances[msg.sender].totalPayments++;
-
         Payment memory newPayment = Payment(
             msg.value,
             block.timestamp,
             msg.sender,
             message
         );
-
         balances[msg.sender].payments[paymentNum] = newPayment;
     }
-
 }
--- smart-contract/test/createOrders.test.js
+++ smart-contract/test/createOrders.test.js
@@ -1,20 +1,38 @@
-const { expect } = require("chai")
-const { expect } = require("hardhat")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Orders", function () {
-    let acc1
-    let acc2
-    let oredersdapl
+describe("Payments", function () {
+  let acc1;
+  let acc2;
+  let payments;
 
-    beforeEach(async function () {
-        [acc1, acc2] = await ethers.getSigners()
-        const Orders = await ethers.getContractFactory("Orders", acc1)
-        oredersdapl = await Orders.deploy()
-        await oredersdapl.deployed()
-        console.log(oredersdapl.address)
-    })
+  beforeEach(async function () {
+    [acc1, acc2] = await ethers.getSigners();
+    const Payments = await ethers.getContractFactory("Payments", acc1);
+    payments = await Payments.deploy();
+    await payments.deployed();
+  });
 
-    it("should be deployed", async function () {
-        console.log("success!")
-    })
-})
+  it("should be deployed", async function () {
+    expect(payments.address).to.be.properAddress;
+  });
+
+  it("should have 0 ether by default", async function () {
+    const balance = await payments.currentBalance();
+    expect(balance).to.eq(0);
+  });
+
+  it("should be possible to send funds", async function () {
+    const sum = ethers.utils.parseEther("0.1"); // Это пример для отправки 0.1 Ether
+    const msg = "hello from hardhat";
+    const tx = await payments.connect(acc2).pay(msg, { value: sum });
+
+    await expect(() => tx).to.changeEtherBalances([acc2, payments], [-sum, sum]);
+    await tx.wait();
+
+    const newPayment = await payments.getPayment(acc2.address, 0);
+    expect(newPayment.message).to.eq(msg);
+    expect(newPayment.amount).to.eq(sum);
+    expect(newPayment.from).to.eq(acc2.address);
+  });
+});
